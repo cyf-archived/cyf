1bf4abc9bd31593ee44b26998bd3c02ba4e8cdb7
kjxbyz <kjxbyz@163.com>
2019-08-30 19:12:40 +0800

    Site updated: 2019-08-30 19:12:39

--- js/schemes/muse.js
+++ js/schemes/muse.js
@@ -1,9 +1,1 @@
-$(document).ready(function() {
-  function updateFooterPosition() {
-    var containerHeight = $('#footer').attr('position') ? $('.container').height() + $('#footer').outerHeight(true) : $('.container').height();
-    if (containerHeight < window.innerHeight) $('#footer').css({ 'position': 'fixed', 'bottom': 0, 'left': 0, 'right': 0 }).attr('position', 'fixed');
-    else $('#footer').removeAttr('style position');
-  }
-  updateFooterPosition();
-  $(window).on('resize scroll', updateFooterPosition);
-});
+$(document).ready(function(){function o(){($("#footer").attr("position")?$(".container").height()+$("#footer").outerHeight(!0):$(".container").height())<window.innerHeight?$("#footer").css({position:"fixed",bottom:0,left:0,right:0}).attr("position","fixed"):$("#footer").removeAttr("style position")}o(),$(window).on("resize scroll",o)});
