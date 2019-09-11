ce9d0b1f239c76072daf6a55f00c49a089ebe229
kjxbyz <kjxbyz@163.com>
2019-09-11 09:24:08 +0800

    Site updated: 2019-09-11 09:23:39

---
+++ js/schemes/muse.js
@@ -0,0 +1,1 @@
+$(document).ready(function(){function o(){($("#footer").attr("position")?$(".container").height()+$("#footer").outerHeight(!0):$(".container").height())<window.innerHeight?$("#footer").css({position:"fixed",bottom:0,left:0,right:0}).attr("position","fixed"):$("#footer").removeAttr("style position")}o(),$(window).on("resize scroll",o)});
---
+++ js/scroll-cookie.js
@@ -0,0 +1,1 @@
+$(document).ready(function(){var o,i=window.location.href.replace(window.location.origin,"");if($(window).on("scroll",function(){clearTimeout(o),o=setTimeout(function(){Cookies.set("scroll-cookie",$(window).scrollTop()+"|"+i,{expires:365,path:""})},250)}),void 0!==Cookies.get("scroll-cookie")){var e=Cookies.get("scroll-cookie").split("|");e[1]===i&&$(window).scrollTop(e[0])}});
