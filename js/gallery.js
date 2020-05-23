93960f57451202f21a9255761dc42a14c52c2e67
kjxbyz (from Travis CI) <kjxbyz@163.com>
2020-05-23 09:01:00 +0000

    Deploy chenyifaerfans/chenyifaerfans to github.com/chenyifaerfans/chenyifaerfans.git:gh-pages

---
+++ js/gallery.js
@@ -0,0 +1,1 @@
+document.addEventListener("DOMContentLoaded",function(){"function"==typeof $.fn.lightGallery&&$(".article").lightGallery({selector:".gallery-item"}),"function"==typeof $.fn.justifiedGallery&&($(".justified-gallery > p > .gallery-item").length&&$(".justified-gallery > p > .gallery-item").unwrap(),$(".justified-gallery").justifiedGallery())});
